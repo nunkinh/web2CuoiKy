803249ac9beeb1b71a23b20aea81340685e05849
nunkinh <nunkinh@gmail.com>
2017-06-19 23:22:35 +0700

    update phan trang

---
+++ web2_cuoiky/web2_cuoiky/SmartShop/Areas/Admin/ViewModels/ProductViewModels.cs
@@ -0,0 +1,19 @@
+﻿using SmartShopConnection;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using SmartShopBUS;
+using SmartShop;
+
+namespace SmartShop.Areas.Admin.ViewModels
+{
+    public class ProductViewModels : Product
+    {
+        [Display(Name="Hình sản phẩm")]
+        [DataType(DataType.Upload)]
+        public HttpPostedFileBase ImageUpload { get; set; }
+        public IEnumerable<Loai> dsLoai1 { get; set; }
+    }
+}
